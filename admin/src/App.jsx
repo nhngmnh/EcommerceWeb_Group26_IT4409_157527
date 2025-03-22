6042dff49e68cf83c6eb594c3f31d5af27349d78
Minh <nhungocminh2004@gmail.com>
2025-03-22 10:38:00 +0700

    Login pages -tuan1

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,12 @@
+import { ToastContainer } from "react-toastify";
+import Login from "./pages/Login";
+import { Route, Routes } from "react-router-dom";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div><ToastContainer/>
+    <Login/>
+    </div>
+    
   )
 }
