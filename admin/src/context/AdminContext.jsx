832ecdf6013e3b07b21c13221d9bd7d72af518ec
Minh <nhungocminh2004@gmail.com>
2025-04-01 23:25:51 +0700

    fix admin context

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -8,10 +8,13 @@ const AdminContextProvider=(props)=>{
     const [aToken,setAToken]=useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
     const [products,setProducts]=useState([])
     const [dashData, setDashData]=useState(false)
+    const [carts, setCarts]=useState([])
+    const [latestComments, setLatestComments]=useState([])
     const backendurl=import.meta.env.VITE_BACKEND_URL
     const getProducts=async()=>{
         try {
-            const{data}=await axios.post(backendurl+'/api/admin/get-products',{},{headers:{aToken}})
+            const { data } = await axios.get(backendurl + '/api/admin/all-products', { headers: { aToken } });
+
             if (data.success){
                 setProducts(data.products)
                 console.log(data.products);
@@ -23,11 +26,26 @@ const AdminContextProvider=(props)=>{
             toast.error(error.message)
         }
     }
-    const changeAvailability= async(itemId)=>{
+    const getCarts =async(req,res)=>{
         try {
-            const {data}=await axios.post(backendurl+ '/api/admin/change-availability',{itemId},{headers:{aToken}})
-            if (data.success){
-                toast.success(data.message)
+            const {data}=await get(backendurl+'/api/admin/all-carts',{headers:{aToken}})
+            if (data){
+                res.json({success:true,carts:data.carts})
+                setCarts(data.carts)
+            } else {
+                res.status(400).json({success:false,message:data.message})
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    }
+    const getLatestComment= async(req,res)=>{
+        try {
+            const {data}=await axios.get(backendurl+'/api/admin/latest-comment',{headers:{aToken}})
+            if (data){
+                res.json({success:true,latestComment:data.latestComment});
+                setLatestComments(data.latestComment)
             } else {
                 toast.error(data.message)
             }
@@ -35,9 +53,25 @@ const AdminContextProvider=(props)=>{
             toast.error(error.message)
         }
     }
+    const changeAvailability= async(itemId)=>{
+        try {
+            const {data}=await axios.post(backendurl+ '/api/admin/change-product-availability',{itemId},{headers:{aToken}})
+            if (data.success){
+                toast.success(data.message)
+            } else {
+                console.log(data.message);
+                
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error);
+            
+            toast.error(error.message)
+        }
+    }
     const getDashData= async()=>{
         try {
-            const {data}=await axios.get(backendurl+'/api/admin/dashboard',{headers:{aToken}})
+            const {data}=await axios.get(backendurl+'/api/admin/admin-dashboard',{headers:{aToken}})
             if (data.success){
                 setDashData(data.dashData)
                 console.log(data.dashData);
@@ -51,9 +85,12 @@ const AdminContextProvider=(props)=>{
     }
     const value={
         aToken,setAToken,
-        backendurl,products,
+        backendurl,products,setProducts,
         getProducts,changeAvailability,
-        dashData,getDashData,setDashData
+        dashData,getDashData,setDashData,
+        carts, setCarts,
+        latestComments, setLatestComments,
+        getCarts, getLatestComment
     }
     return (
         <AdminContext.Provider value={value}>
