7aa44cb548d18b96d3318ef68257c902d1f9590b
kostromin-s <namly6702@gmail.com>
2025-05-12 06:42:25 +0700

    update userController (registerUser,loginUser)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,102 +1,62 @@
+import userModel from "../models/userModel.js";
 import validator from "validator";
 import bcrypt from "bcrypt";
-import userModel from "../model/userModel.js";
 import jwt from "jsonwebtoken";
-import { json } from "express";
 
-const createToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECE);
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.json({ success: false, message: "Missing Details" }); // missing sth
+    }
+    if (!validator.isEmail(email)) {
+      // invalid email
+      return res.json({ success: false, message: "Invalid email" });
+    }
+    if (password.length < 8) {
+      //weak password
+      return res.json({
+        success: false,
+        message: "Please enter strong password",
+      });
+    }
+    // HAShing USER PASSWORD
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const userData = {
+      name: username,
+      email,
+      password: hashedPassword,
+    };
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    return res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    return res.json({ success: false, message: error.message });
+  }
 };
+//API for user login
 
 const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
-
     const user = await userModel.findOne({ email });
-
     if (!user) {
-      return res.json({
-        success: false,
-        message: "Mày đã chắc là mày nhập đúng email chưa",
-      });
+      return res.json({ success: false, message: "User do not exist" });
     }
-
     const isMatch = await bcrypt.compare(password, user.password);
-
     if (isMatch) {
-      const token = createToken(user._id);
-      res.json({ success: true, token });
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      return res.json({ success: true, token });
     } else {
-      res.json({ success: false, message: "Sai mật khẩu rồi ba!" });
-    }
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-const registerUser = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    const exists = await userModel.findOne({ email });
-
-    if (exists) {
-      return res.json({
-        success: false,
-        message: "Tên tài khoản đã được sử dụng",
-      });
-    }
-    if (!validator.isEmail(email)) {
-      return res.json({ success: false, message: "Email không hợp lệ" });
-    }
-
-    if (!name || name.trim() === "") {
-      return res.json({ success: false, message: "Tên không được để trống" });
-    }
-
-    if (password.length < 8) {
-      return res.json({ success: false, message: "Mật khẩu chưa đủ wow!" });
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const newUser = new userModel({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    const user = await newUser.save();
-
-    const token = createToken(user._id);
-
-    res.json({ success: true, token });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-const adminLogin = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    if (
-      email === process.env.ADMIN_EMAIL &&
-      password === process.env.ADMIN_PASSWORD
-    ) {
-      const token = jwt.sign(email + password, process.env.JWT_SECE);
-      return res.status(200).json({ success: true, token });
-    } else {
-      return res
-        .status(404)
-        .json({ success: false, message: "Không phải admin" });
+      return res.json({ success: false, message: "Invalid credentials" });
     }
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ success: false, message: error.message });
+    return res.json({ success: false, message: error.message + "hehehe" });
   }
 };
 
-export { loginUser, registerUser, adminLogin };
+//api get user profile
